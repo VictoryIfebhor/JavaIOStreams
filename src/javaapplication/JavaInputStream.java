21665f6ea05a834c2b0f8615195d231ce5a93f1a
Victory Ifebhor <victory.ifebhor@gmail.com>
2021-04-26 16:20:56 -0900

    Made adjustments to the JavaInputStream class to handle errors better

--- src/javaapplication/JavaInputStream.java
+++ src/javaapplication/JavaInputStream.java
@@ -6,8 +6,11 @@
 package javaapplication;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,36 +18,48 @@ import java.io.InputStream;
  */
 
 public class JavaInputStream {
-    public static void main(String[] args) throws IOException {
-        /* first create an input stream
+    public static void main(String[] args){
+        // step1
+        /* first create an input stream using a try-with context.
+         * a try-with context will create the stream and automatically close it ones the try-with block is run.
          * pass in the path to the file you want to read from
-        
          * Note that the path I used is relative to my current working directory (in this case, the project folder)
          * you can use a valid path to any text file you have on your computer.
          * Also note that I have manually created this file in that path and write "HELLO WORLD" in it.
          * I am going to read the content of the file and print it.
          */
         
-        InputStream inputStream = new FileInputStream("files/hello world.txt");
-        
+        // step2
         /* create a byte array that will store the bytes read from the input stream
-         * to instantiate a new byte array, the size of the array is required.
-         * so I an estimate of the number of bytes that can be read from the input stream using the 'available' method 
-         * then I used the estimate gotten as the size of the array.
-         */
-        int size = inputStream.available();
-        byte[] byteArray = new byte[size];
+        * to instantiate a new byte array, the size of the array is required.
+        * so I an estimate of the number of bytes that can be read from the input stream using the 'available' method
+        * then I used the estimate gotten as the size of the array.
+        */
         
+        // step3
         // after creating the byte array, read the bytes from the input stream and store in the array.
-        inputStream.read(byteArray);
         
+        // step4
         /* The input stream has been used to read the content of the file and store in the array successfully.
          * The array can then be used to create a string whose value will be the content of the file.
          */
-        String content = new String(byteArray);
-        System.out.println(content);
+            //step1
+            try ( InputStream inputStream = new FileInputStream("files/hello world.txt")) {
+                
+                //step2
+                int size = inputStream.available();
+                byte[] byteArray = new byte[size];
+                
+                //step3
+                inputStream.read(byteArray);
+                
+                //step4
+                String content = new String(byteArray);
+                System.out.println(content);
+            }
+         catch (IOException ex) {
+            Logger.getLogger(JavaInputStream.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
-        // close the input stream
-        inputStream.close();
     }
 }
