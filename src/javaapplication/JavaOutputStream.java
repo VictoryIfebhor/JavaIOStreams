4f051a6ffac3d742a13d064c53803d2976c5851d
Victory Ifebhor <victory.ifebhor@gmail.com>
2021-04-29 15:31:25 -0900

    Removed imports that are not used

--- src/javaapplication/JavaOutputStream.java
+++ src/javaapplication/JavaOutputStream.java
@@ -5,12 +5,9 @@
  */
 package javaapplication;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
